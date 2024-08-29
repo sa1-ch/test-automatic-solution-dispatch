364e2059ebbd9558d800a38115b27feecf24d547
sai.chodapaneedi <sai.chodapaneedi@tigeranalytics.com>
2024-08-29 12:15:24 +0530

    Added 1st phase

---
+++ templates/mwodda-snacking-solution-config-template/development/tf_config/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    tenant_id            = "2fc13e34-f03f-498b-982a-7cb446e25bc6"
+    subscription_id      = "c6bfc6b3-5e5e-45f8-b6b7-2f8f1c8a1a0e"
+    resource_group_name  = "ODDA-TFSTATE-DEV-RG"
+    storage_account_name = "oddatfstateeus2devsa"
+    container_name       = "resource-tfstate"
+    key                  = "ODDA1-{solution_pillar_name}{solution_name}-EUS2"
+  }
+}
